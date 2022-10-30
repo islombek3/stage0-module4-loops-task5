f15aee496ba00705a14c2767706f95d244d65d92
Islombek Akhmadjonov <81472194+islombek3@users.noreply.github.com>
2022-10-30 16:02:49 +0500

    add logic

--- src/main/java/school/mjc/stage0/loops/task5/Triangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Triangle.java
@@ -2,6 +2,8 @@ package school.mjc.stage0.loops.task5;
 
 public class Triangle {
     public void printTriangle(int cathetusLength) {
-
+        String eight = "8";
+        for (int i = 1; i <= cathetusLength; i++)
+            System.out.println(eight.repeat(i));
     }
 }
