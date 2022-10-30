2d5766f6ff07225c382e27c995d183651c65d444
Islombek Akhmadjonov <81472194+islombek3@users.noreply.github.com>
2022-10-30 16:00:39 +0500

    add logic

--- src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
@@ -2,6 +2,13 @@ package school.mjc.stage0.loops.task5;
 
 public class Rectangle {
     public void printRectangleFrom8s(int length, int height){
-
+        for (int i = 0; i < height; i++){
+            String eight = "8";
+            String empty = " ";   
+            if (i == 0 || i == height-1)
+                System.out.println(eight.repeat(length));
+            else
+                System.out.println(eight + empty.repeat(length - 2) + eight);
+        }
     }
 }
