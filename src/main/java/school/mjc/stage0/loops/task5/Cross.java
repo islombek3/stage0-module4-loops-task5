6e1f73cf6b7a23c902dcf2a027d607c87ba92fc5
Islombek Akhmadjonov <81472194+islombek3@users.noreply.github.com>
2022-10-30 15:58:31 +0500

    add logic

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -2,6 +2,15 @@ package school.mjc.stage0.loops.task5;
 
 public class Cross {
     public void printCross(int sideLength) {
-
+        String eight = "8";
+        String empty = " ";
+        int median = (int)Math.ceil(sideLength/2);
+        String emptySpace = empty.repeat(median);
+        for (int i = 0; i < sideLength; i++){
+            if (i != median)
+                System.out.println(emptySpace + eight + emptySpace);
+            else
+                System.out.println(eight.repeat(sideLength));
+        }
     }
 }
