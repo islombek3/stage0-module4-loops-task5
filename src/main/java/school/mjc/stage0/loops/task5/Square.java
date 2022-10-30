7d2b5dc09d509bc56ab60cd7700c73fbb749b4ec
Islombek Akhmadjonov <81472194+islombek3@users.noreply.github.com>
2022-10-30 16:04:52 +0500

    fix logic

--- src/main/java/school/mjc/stage0/loops/task5/Square.java
+++ src/main/java/school/mjc/stage0/loops/task5/Square.java
@@ -2,13 +2,13 @@ package school.mjc.stage0.loops.task5;
 
 public class Square {
     public void printSquareFrom8s(int sideLength){
-        for (int i = 0; i < height; i++){
+        for (int i = 0; i < sideLength; i++){
             String eight = "8";
             String empty = " ";   
-            if (i == 0 || i == height-1)
-                System.out.println(eight.repeat(length));
+            if (i == 0 || i == sideLength-1)
+                System.out.println(eight.repeat(sideLength));
             else
-                System.out.println(eight + empty.repeat(length - 2) + eight);
+                System.out.println(eight + empty.repeat(sideLength - 2) + eight);
         }
     }
 }
