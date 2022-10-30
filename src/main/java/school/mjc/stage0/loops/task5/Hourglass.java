12cd0c75811c946daa40cd136c0459ee5e7c2e2d
Islombek Akhmadjonov <81472194+islombek3@users.noreply.github.com>
2022-10-30 15:59:31 +0500

    add logic

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,21 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
-
+        String eight = "8";
+        String empty = " ";
+        int i = 0;
+        int x = height;
+        while (x >= 1){
+            System.out.println(empty.repeat(i) + eight.repeat(x) + empty.repeat(i));
+            i++;
+            x -= 2;
+        }
+        x = height % 2 == 0? x+2: x+4;
+        i = height % 2 == 0? i-1: i-2;
+        while (x <= height){
+            System.out.println(empty.repeat(i) + eight.repeat(x) + empty.repeat(i));
+                i--;
+                x += 2;
+        }
     }
 }
